f1e5c7cad63290769716dc6cdefdd41872720e19
Pedro Vitor <pedrovitorti@gmail.com>
2018-05-15 19:43:29 -0300

    imageshow.php ok

--- paginas/exibir_avisos/imageshow.php
+++ paginas/exibir_avisos/imageshow.php
@@ -44,7 +44,7 @@
 
 	var n=1;
 	function exibir(){
-		for (i = 0; i < 6; i++) { 
+		for (i = 0; i < <?php echo $_SESSION['qtd_avisos']?>; i++) { 
 
 			var novoItem = '<img src="http://avisos.sic-maracanau.com.br/paginas/uploads/img'+n+'.jpg"  height=100% width=100%>';
 			$('#slideshow').append(novoItem);
