93c90dcb823c050984ce092849c8d5786830e8f5
Pedro Vitor <pedrovitorti@gmail.com>
2018-07-03 13:56:44 -0300

    Adicionando metódo MQTT no sistema. Modificando: config_db/enviar.php, config_db/phpMQTT.php

--- config_bd/enviar.php
+++ config_bd/enviar.php
@@ -114,21 +114,42 @@ for ($i = 1; $i <= $_SESSION['qtd_avisos']; $i ++) {
  */
 
 // alterando arquivo.txt
-$f = fopen("../paginas/arquivo.txt", "r+"); // Abre para leitura e gravação; coloca o ponteiro no começo do arquivo.
+
+$f = fopen("../../site_principal/arquivo.txt", "r+"); // Abre para leitura e gravação; coloca o ponteiro no começo do arquivo.
 $text = 'b';
 fwrite($f, $text);
 fclose($f);
 
+// inicio mqtt
+
+//sleep(10); // ??? Para ter certeza que deu tempo as novas imagens subirem para o servidor
+
+require("phpMQTT.php");
+
+$server = "ifce.sanusb.org";     // change if necessary
+$port = 1883;                     // change if necessary
+$username = "";                   // set your username
+$password = "";                   // set your password
+$client_id = "pedro-MQTTx"; // make sure this is unique for connecting to sever - you could use uniqid()
+
+$mqtt = new phpMQTT($server, $port, $client_id);
+
+if ($mqtt->connect(true, NULL, $username, $password)) {
+	$mqtt->publish("/seci", "true", 0);
+	$mqtt->close();
+}
+
+// fim mqtt
+
 sleep(10); // Espera 10 segundos para dá tempo o raspberry atualizar os avisos, tempo muito pegueno pode não atualizar
            
 // alterando arquivo.txt
-$f = fopen("../paginas/arquivo.txt", "r+");
+$f = fopen("../../site_principal/arquivo.txt", "r+");
 $text = 'a';
 fwrite($f, $text);
 fclose($f);
 header("Location:../paginas/principal_adm.php");
 ?>
-
 
 
 
