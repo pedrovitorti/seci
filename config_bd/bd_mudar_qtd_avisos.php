fd76f53731c19f226d10b706b1885ec02c1e75a4
Pedro Vitor <pedrovitorti@gmal.com>
2018-07-05 22:11:37 -0300

    mqtt em mudar qtd avisos está ok

--- config_bd/bd_mudar_qtd_avisos.php
+++ config_bd/bd_mudar_qtd_avisos.php
@@ -5,7 +5,30 @@ $qtd = $_POST['qtd'];
 
 $sql1 = pg_query("UPDATE qtd_avisos SET quantidade ='$qtd';") or die("Erro no comando SQL");
 
+
+// inicio mqtt
+
+sleep(10); // ??? Para ter certeza que deu tempo as novas imagens subirem para o servidor
+
+require("phpMQTT.php");
+
+$server = "ifce.sanusb.org";     // change if necessary
+$port = 1883;                     // change if necessary
+$username = "";                   // set your username
+$password = "";                   // set your password
+$client_id = "pedro-MQTTx"; // make sure this is unique for connecting to sever - you could use uniqid()
+
+$mqtt = new phpMQTT($server, $port, $client_id);
+
+if ($mqtt->connect(true, NULL, $username, $password)) {
+    $mqtt->publish("/seci", "atualizar", 0);
+    $mqtt->close();
+}
+
+// fim mqtt
 // alterando arquivo.txt
+
+/*
 $f = fopen("../../site_principal/arquivo.txt", "r+"); // Abre para leitura e gravação; coloca o ponteiro no começo do arquivo.
 $text = 'b';
 fwrite($f, $text);
@@ -18,6 +41,7 @@ $f = fopen("../../site_principal/arquivo.txt", "r+");
 $text = 'a';
 fwrite($f, $text);
 fclose($f);
+*/
 header("Location:../paginas/numero_avisos.php");
 
 ?>
